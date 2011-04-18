58d83dcf9e581af0c4e417f1d87e73c678f5c91c
guns <self@sungpae.com>
2011-04-18 17:21:13 -0500

    New project tree

---
+++ src/lib/haus/task.rb
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+
+class Haus
+  class Task
+    class << self
+    end
+
+    def initialize args = []
+      @args = args
+    end
+
+    def options
+    end
+
+    def call
+    end
+  end
+end
---
+++ src/lib/haus/version.rb
@@ -0,0 +1,5 @@
+# -*- encoding: utf-8 -*-
+
+module Haus
+  VERSION = '0.7.0'
+end
