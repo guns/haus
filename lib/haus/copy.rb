d24f68f96ac73e307827cc845f8cc0409eeeff16
guns <self@sungpae.com>
2011-08-01 19:09:57 -0500

    Subclass Task, not Link

--- lib/haus/copy.rb
+++ lib/haus/copy.rb
@@ -1,9 +1,9 @@
 # -*- encoding: utf-8 -*-
 
-require 'haus/link'
+require 'haus/task'
 
 class Haus
-  class Copy < Link
+  class Copy < Task
     desc 'Copy dotfiles'
     help "#{Options.new.path}/etc/*"
 
@@ -13,6 +13,12 @@ class Haus
           queue.add_copy src, user.dotfile(src)
         end
       end
+    end
+
+    def call args = []
+      queue.options = options
+      enqueue
+      queue.execute
     end
   end
 end
