3c493307222e68950c51f48063a2b903e3f170d8
guns <self@sungpae.com>
2011-08-01 19:47:12 -0500

    Help

--- lib/haus/copy.rb
+++ lib/haus/copy.rb
@@ -5,7 +5,7 @@ require 'haus/task'
 class Haus
   class Copy < Task
     desc 'Copy dotfiles'
-    help "#{Options.new.path}/etc/*"
+    help "Copy dotfiles from #{Options.new.path}/etc/*"
 
     def enqueue
       users.each do |user|
