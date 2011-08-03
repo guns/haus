e76d123e7c7fe6e1ff254dd018f85b825de4990c
guns <self@sungpae.com>
2011-08-02 20:59:35 -0500

    Fix LOAD_PATH munging in all tests

--- lib/haus/test/task_options.test.rb
+++ lib/haus/test/task_options.test.rb
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 
-$:.unshift File.expand_path('../../lib', __FILE__)
+$:.unshift File.expand_path('../../..', __FILE__)
 
 require 'rubygems' # 1.8.6 compat
 require 'minitest/pride' if $stdout.tty? and [].respond_to? :cycle
