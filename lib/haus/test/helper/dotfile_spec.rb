e15fe82d787caf672a5d640180ec7b321a493987
guns <self@sungpae.com>
2011-08-04 18:51:30 -0500

    Extract common tests from copy and link

---
+++ lib/haus/test/helper/dotfile_spec.rb
@@ -0,0 +1,81 @@
+# -*- encoding: utf-8 -*-
+
+require 'rubygems' # 1.8.6 compat
+require 'minitest/pride' if $stdout.tty? and [].respond_to? :cycle
+require 'minitest/autorun'
+require 'haus/test/helper/test_user'
+
+class DotfileSpec < MiniTest::Spec
+  def create_task klass
+    @task = klass.new
+    @task.options.force = true
+    @task.options.quiet = true
+  end
+
+  def remove_task_archive
+    FileUtils.rm_f @task.queue.archive_path
+  end
+
+  def add_task_jobs_to_queue type
+    user = Haus::TestUser[@task.class.to_s + '_enqueue']
+    jobs = [:file, :dir, :link].inject({}) { |h,m| h.merge Hash[*user.hausfile(m)] } # Ruby 1.8.6
+
+    @task.options.path = user.haus
+    @task.options.users = [user.name]
+    @task.enqueue
+    @task.queue.send(type).map { |s,d| s }.sort.must_equal jobs.keys.sort
+    @task.queue.targets.sort.must_equal jobs.values.sort
+  end
+
+  def pass_options_to_queue_before_enqueueing
+    @task.instance_eval do
+      def enqueue *args
+        queue.options.cow.must_equal 'MOOCOW'
+        super
+      end
+    end
+
+    @task.options.cow = 'MOOCOW'
+    @task.run
+    @task.queue.options.cow.must_equal 'MOOCOW'
+  end
+
+  def pass_options_to_queue_before_execution
+    @task.instance_eval do
+      def execute *args
+        queue.options.cow.must_equal 'MOOCOW'
+        super
+      end
+    end
+
+    @task.options.cow = 'MOOCOW'
+    @task.run
+    @task.queue.options.cow.must_equal 'MOOCOW'
+  end
+
+  def execute_the_queue
+    @task.queue.executed?.must_equal nil
+    @task.run
+    @task.queue.executed?.must_equal true
+  end
+
+  def return_true_or_nil
+    @task.options.quiet = true
+    @task.options.force = true
+    @task.run.must_equal true
+    @task.options.force = false
+    @task.run.must_equal nil
+  end
+
+  def should_result_in_dotfiles
+    user = Haus::TestUser[@test.class.to_s + '_dotfiles']
+    jobs = [:file, :dir, :link].inject({}) { |h,m| h.merge m => user.hausfile(m) } # Ruby 1.8.6 Hash[] does not like 2D arrays
+    jobs.each_value { |s,d| File.exists?(d).must_equal false }
+
+    @task.options.path = user.haus
+    @task.options.users = [user.name]
+    @task.run
+
+    yield jobs
+  end
+end
--- lib/haus/test/link.test.rb
+++ lib/haus/test/link.test.rb
@@ -3,21 +3,16 @@
 $:.unshift File.expand_path('../../..', __FILE__)
 
 require 'fileutils'
-require 'rubygems' # 1.8.6 compat
-require 'minitest/pride' if $stdout.tty? and [].respond_to? :cycle
-require 'minitest/autorun'
 require 'haus/link'
-require 'haus/test/helper/test_user'
+require 'haus/test/helper/dotfile_spec'
 
-class Haus::LinkSpec < MiniTest::Spec
+class Haus::LinkSpec < DotfileSpec
   before do
-    @link = Haus::Link.new
-    @link.options.force = true
-    @link.options.quiet = true
+    create_task Haus::Link
   end
 
   after do
-    FileUtils.rm_f @link.queue.archive_path
+    remove_task_archive
   end
 
   describe :options do
@@ -30,71 +25,33 @@ class Haus::LinkSpec < MiniTest::Spec
 
   describe :enqueue do
     it 'should add link jobs to the queue' do
-      user = Haus::TestUser[:link_enqueue]
-      jobs = [:file, :dir, :link].inject({}) { |h,m| h.merge Hash[*user.hausfile(m)] } # Ruby 1.8.6
-
-      @link.options.path = user.haus
-      @link.options.users = [user.name]
-      @link.enqueue
-      @link.queue.links.map { |s,d| s }.sort.must_equal jobs.keys.sort
-      @link.queue.targets.sort.must_equal jobs.values.sort
+      add_task_jobs_to_queue :links
     end
   end
 
   describe :run do
     it 'should pass options to queue before enqueueing files' do
-      @link.instance_eval do
-        def enqueue *args
-          queue.options.cow.must_equal 'MOOCOW'
-          super
-        end
-      end
-
-      @link.options.cow = 'MOOCOW'
-      @link.run
-      @link.queue.options.cow.must_equal 'MOOCOW'
+      pass_options_to_queue_before_enqueueing
     end
 
     it 'should pass options to queue before execution' do
-      @link.instance_eval do
-        def execute *args
-          queue.options.cow.must_equal 'MOOCOW'
-          super
-        end
-      end
-
-      @link.options.cow = 'MOOCOW'
-      @link.run
-      @link.queue.options.cow.must_equal 'MOOCOW'
+      pass_options_to_queue_before_execution
     end
 
     it 'should execute the queue' do
-      @link.queue.executed?.must_equal nil
-      @link.run
-      @link.queue.executed?.must_equal true
-    end
-
-    it 'should link all sources as dotfiles' do
-      user = Haus::TestUser[:link_run]
-      jobs = [:file, :dir, :link].inject({}) { |h,m| h.merge Hash[*user.hausfile(m)] } # Ruby 1.8.6
-      jobs.values.each { |f| File.exists?(f).must_equal false }
-
-      @link.options.path = user.haus
-      @link.options.users = [user.name]
-      @link.run
-
-      jobs.values.each do |f|
-        # Ruby 1.9 has Hash#key
-        File.readlink(f).must_equal jobs.find { |s,d| d == f }.first
-      end
+      execute_the_queue
     end
 
     it 'should return true or nil' do
-      @link.options.quiet = true
-      @link.options.force = true
-      @link.run.must_equal true
-      @link.options.force = false
-      @link.run.must_equal nil
+      return_true_or_nil
+    end
+
+    it 'should link all sources as dotfiles' do
+      should_result_in_dotfiles do |jobs|
+        jobs.each_value do |s,d|
+          File.readlink(d).must_equal s
+        end
+      end
     end
   end
 end
