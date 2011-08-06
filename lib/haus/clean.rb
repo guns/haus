1f4c5bacbcdfe90265b964ec83e87e389e861668
guns <self@sungpae.com>
2011-08-06 02:23:38 -0500

    Tasks must raise, not abort

--- lib/haus/clean.rb
+++ lib/haus/clean.rb
@@ -37,7 +37,7 @@ class Haus
 
     def run
       args = super
-      abort options.to_s if args.size > 0
+      raise options.to_s if args.size > 0
       queue.options = options
       enqueue
       queue.execute
