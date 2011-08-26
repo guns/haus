0b76174d2851082a3c760a5a388b47e5a1d744ef
guns <self@sungpae.com>
2011-08-26 03:08:06 -0500

    Change User#trusts? to more useful User#distrusts
    e.g.
    
        if reason = user.distrusts(file)
          abort reason
        end

--- lib/haus/test/user.test.rb
+++ lib/haus/test/user.test.rb
@@ -51,39 +51,50 @@ class Haus::UserSpec < MiniTest::Spec
     end
   end
 
-  describe :trusts? do
+  describe :distrusts do
     before do
       @user = Haus::User.new $user.uid
     end
 
     it 'must accept one argument' do
-      @user.method(:trusts?).arity.must_equal 1
+      @user.method(:distrusts).arity.must_equal 1
     end
 
     it 'must raise ENOENT when given path does not exist' do
-      lambda { @user.trusts? '/perfect/child' }.must_raise Errno::ENOENT
+      lambda { @user.distrusts '/perfect/child' }.must_raise Errno::ENOENT
     end
 
-    it 'must return false when a path is not owned by the user or by root' do
-      # Test root and current user (non-test!) home directories
-      [0, Etc.getlogin].map { |n| Haus::User.new n }.each do |user|
-        path = user.dir
-        stat = File.lstat path
-        stat.ftype.must_equal 'directory'
-        stat.uid.must_equal user.uid
-        (stat.mode & 0022).must_equal 0
-        @user.trusts?(path).must_equal stat.uid.zero?
-      end
+    it 'must return a reason when path is not owned by user or root, is group-writable, or is world-writable' do
+      # Non-test-user owned directory
+      home_path = File.expand_path '~'
+      home_stat = File.stat home_path
+      home_stat.uid.wont_equal @user.uid
+      (home_stat.mode & 0022).must_equal 0
+      @user.distrusts(home_path).must_match /not owned by.*root/
+
+      # Root-owned world-writable directory
+      tmp_path = File.expand_path '/tmp'
+      tmp_stat = File.stat tmp_path
+      tmp_stat.uid.must_equal 0
+      (tmp_stat.mode & 0002).must_equal 0002
+      @user.distrusts(tmp_path).must_match /world writable/
+
+      # Current-user owned group-writable directory
+      user = Haus::User.new Etc.getlogin
+      file_path = Tempfile.new(user.name).path
+      file_stat = File.stat file_path
+      file_stat.uid.must_equal user.uid
+      FileUtils.chmod 0660, file_path
+      user.distrusts(file_path).must_match /group writable/
     end
 
-    it 'must return false when a path is not solely writable by the user or by root' do
-      # Root-owned, world-writable directory
-      path = '/tmp'
-      stat = File.stat path # Not :lstat; /tmp is sometimes a symlink
-      stat.ftype.must_equal 'directory'
-      stat.uid.must_equal 0
-      (stat.mode & 0022).wont_equal 0
-      @user.trusts?(path).must_equal false
+    it 'must return nil when a path is fully trusted' do
+      user = Haus::User.new Etc.getlogin
+      path = Tempfile.new(user.name).path
+      stat = File.stat path
+      stat.uid.must_equal user.uid
+      FileUtils.chmod 0644, path
+      user.distrusts(path).must_be_nil
     end
 
     it 'must check ACLs to see if the path is truly owner-write-only' do
