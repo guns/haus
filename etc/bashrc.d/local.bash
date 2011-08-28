e764248ae9c2f5d81234f91e3f3b934ca8b1a645
guns <self@sungpae.com>
2011-08-28 16:23:38 -0500

    [bash] improve ssh-shell alias

--- etc/bashrc.d/local.bash
+++ etc/bashrc.d/local.bash
@@ -619,7 +619,7 @@ ALIAS ssh='ssh -C -2' \
       ssh-tunnel='ssh -Nn -M -D 22222' \
       ssh-password='ssh -o "PreferredAuthentications password"' \
       ssh-nocompression='ssh -o "Compression no"'
-HAVE ssh-agent && alias ssh-shell="exec ssh-agent \"$SHELL\""
+HAVE ssh-agent && alias ssh-shell="[[ \"\$SSH_AGENT_PID\" ]] || exec ssh-agent \"$SHELL\""
 HAVE ssh-proxy && tcomp ssh ssh-proxy
 ALIAS scp='scp -C -2' \
       scpr='scp -r'
