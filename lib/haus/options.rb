609b75ab2c09ddf4b19ddf6f7faaece7e31ed302
guns <self@sungpae.com>
2011-09-15 10:10:41 -0500

    Options#initialize now maintains funcdef parity
    Parity with OpenStruct#initialize and OptionParser#initialize

--- lib/haus/options.rb
+++ lib/haus/options.rb
@@ -12,12 +12,20 @@ class Haus
   # Options#logger provides default logger
   #
   class Options < OptionParser
-    def initialize
-      @ostruct = OpenStruct.new
-      @ostruct.path = File.expand_path '../../..', __FILE__ # HAUS_PATH
-      @ostruct.debug = !!ENV['DEBUG']
+    def initialize arg = nil, width = nil, indent = nil
+      # Send first arg to @ostruct or self
+      hash   = arg if arg.is_a? Hash
+      banner = arg unless arg.is_a? Hash
+
+      @ostruct        = OpenStruct.new hash
+      @ostruct.path   = File.expand_path '../../..', __FILE__
+      @ostruct.debug  = !!ENV['DEBUG']
       @ostruct.logger = Haus::Logger.new
-      super
+
+      params = [banner]
+      params.push width if width
+      params.push indent if indent
+      super *params
     end
 
     def path= arg
