d9dc10267c9f2eef8bfbfac4050ad9f895303de9
guns <self@sungpae.com>
2012-01-19 04:51:27 -0600

    [clojure] fix

--- etc/%clojure/%src/haus/math.clj
+++ etc/%clojure/%src/haus/math.clj
@@ -1,16 +1,19 @@
 (ns haus.math)
 
-(defn leading-digit [n]
+(defn leading-digit
   "Return the high order digit (base10) of a number; e.g. for Benford's law."
+  [n]
   (let [div (int (Math/pow 10 (int (Math/log10 (int n)))))]
     (int (/ n div))))
 
-(defn counts-by [& args]
+(defn counts-by
   "Like group-by, but values are replaced by their counts."
+  [& args]
   (into {} (for [[k v] (apply group-by args)] [k (count v)])))
 
-(defn pascals-triangle []
+(defn pascals-triangle
   "Return a lazy seq of rows of Pascal's triangle."
+  []
   (letfn [(next-row [row]
             (reduce (fn [v' v] (conj v' (apply +' (take 2 v)))) ; (take 2) returns [1] at the right edge
                     [1]                                         ; Left edge
