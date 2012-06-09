dec417e4722a8a5bb9221bc45af755b7ef0a1a1e
guns <self@sungpae.com>
2012-06-09 02:23:07 -0500

    [leiningen] update vimclojure/server

--- etc/%lein/profiles.clj
+++ etc/%lein/profiles.clj
@@ -19,8 +19,9 @@
 ;;        guns <self@sungpae.com>                    /######  /#
 ;;                                                  /     ###/
 
-{:user {:plugins [[lein-pprint "1.1.1"]
+{:user {:plugins [[vimclojure/server "2.3.2"]
                   [lein-tarsier "0.9.2"]
+                  [lein-pprint "1.1.1"]
                   [lein-outdated "0.1.0"]
                   [jonase/kibit "0.0.3"]
                   [jonase/eastwood "0.0.2"]]}}
