7a3271e442df8dc8f5dd1e913d17cb91134e398d
guns <self@sungpae.com>
2012-06-18 17:21:53 -0500

    [bash] New, slightly less stupid dusort()

--- etc/bashrc.d/local.bash
+++ etc/bashrc.d/local.bash
@@ -389,19 +389,43 @@ alias du='du -h'
 alias dus='du -s'
 # Param: [$@] Files to list
 dusort() {
-    echo >&2 'Calculating sorted file size...'
+    ruby -r shellwords -e '
+        fs = ARGV.empty? ? Dir["*"] : ARGV
+        idx, size,      = -1, fs.count
+        res, pool, lock = [], [], Mutex.new
+        label           = "\r%#{size.to_s.length}d/#{size}"
 
-    local buf f line
+        # We are assuming POSIX 512 bytes per block
+        ENV.delete "BLOCKSIZE"
 
-    if (($#)); then
-        buf="$(for f in "$@"; do du -s "$f"; done)"
-    else
-        buf="$(f1 \( ! -name . \) -print0 | xargs -0 du -s)"
-    fi
+        # Threading mostly for for user feedback.
+        # Will usually be slower than a single call to `du`
+        4.times do
+            pool << Thread.new do
+                loop do
+                    i = lock.synchronize { idx += 1 }
+                    break if i >= size
+                    $stderr.print label % (i+1)
+                    res[i] = [%x(du -s #{fs[i].shellescape})[/^\d+/].to_i * 512, fs[i]]
+                end
+            end
+        end
 
-    echo -e "$buf" | sort -n | cut -f2 | while read line; do
-        command du -sh -- "$line"
-    done
+        pool.each &:join
+        print "\r"
+
+        ps = res.sort_by { |s,f| s }.map do |s,f|
+            case s
+            when 0...2**10     then [                   s.to_s, "B", f]
+            when 2**10...2**20 then ["%.2f" % (s.to_f / 2**10), "K", f]
+            when 2**20...2**30 then ["%.2f" % (s.to_f / 2**20), "M", f]
+            else                    ["%.2f" % (s.to_f / 2**30), "G", f]
+            end
+        end
+
+        fmt = "%#{ps.map { |s,u,f| s.length }.max}s %s  %s"
+        ps.each { |p| puts fmt % p }
+    ' "$@"
 }
 
 # mount
