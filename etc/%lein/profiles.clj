09651967f41c2dcbf056c21204680726284d2df0
guns <self@sungpae.com>
2012-07-28 05:44:57 -0500

    [leiningen] Update

--- etc/%lein/profiles.clj
+++ etc/%lein/profiles.clj
@@ -20,8 +20,4 @@
 ;;                                                  /     ###/
 
 {:user {:plugins [[vimclojure/server "2.3.3"]
-                  [lein-tarsier "0.9.2"]
-                  [lein-pprint "1.1.1"]
-                  [lein-outdated "0.1.0"]
-                  [jonase/kibit "0.0.3"]
-                  [jonase/eastwood "0.0.2"]]}}
+                  [lein-tarsier "0.9.2"]]}}
