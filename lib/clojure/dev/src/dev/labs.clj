c949d81b0be56f2aeaf72c715f655305a40c4194
guns <self@sungpae.com>
2012-07-29 16:37:58 -0500

    [lib/clojure] dev.labs/defn+

---
+++ lib/clojure/dev/src/dev/labs.clj
@@ -0,0 +1,18 @@
+(ns dev.labs)
+
+(defmacro defn+
+  "clojure.core/defn with more destructuring sugar."
+  [& forms]
+  (let [[l r] (split-with (comp not vector?) forms)
+        [lb rb] (split-with #(not= '& %) (first r))
+        rb' (map (fn [e]
+                   (cond
+                     ;; (defn+ λ [& {a 1 b 2}])
+                     (and (map? e) (every? symbol? (keys e)))
+                     {:keys (keys e) :or e}
+                     ;; (defn+ λ [& {:keys {a 1 b 2}}])
+                     (and (contains? e :keys) (map? (:keys e)))
+                     (assoc e :keys (keys (:keys e)) :or (:keys e))
+                     ;; Pass through
+                     :else e)) rb)]
+    `(defn ~@(concat l (cons (vec (concat lb rb')) (rest r))))))
