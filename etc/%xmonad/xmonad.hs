98fb0a13a86aefeaae7ca2005cfd873bc340262c
guns <self@sungpae.com>
2012-09-04 04:37:46 -0500

    [subtle] [xmonad] Goodbye tiling WMs
    Tmux is all the tiling I need.
    
    Next up, e17

--- etc/%xmonad/xmonad.hs
+++
@@ -1,9 +0,0 @@
-import XMonad
-
-main = do
-    xmonad $ defaultConfig
-        { modMask           = controlMask .|. mod4Mask
-        , terminal          = "rxvt-unicode --client"
-        , borderWidth       = 0
-        , focusFollowsMouse = False
-        }
