c140c1d1fb3b925d2c139afc8ddd8f84c928ec49
guns <self@sungpae.com>
2012-09-15 09:12:27 -0500

    [bash] [readline] No /usr/local/src

--- etc/bashrc.d/interactive.bash
+++ etc/bashrc.d/interactive.bash
@@ -165,7 +165,6 @@ CD_FUNC cdhome          ~
 CD_FUNC cdclojure       ~/.clojure /opt/clojure
 CD_FUNC cdhaus          ~/.haus /opt/haus && export cdhaus RUBYLIB="$cdhaus/lib/ruby"
 CD_FUNC cdsrc           ~/src ~guns/src /usr/local/src
-CD_FUNC cdSRC           /usr/local/src
 CD_FUNC cdvimfiles      "$cdsrc/vimfiles"
 CD_FUNC cdmetasploit    "$cdsrc/metasploit" && export cdmetasploit # Export for vim autocmd
 CD_FUNC cddownloads     ~/Downloads
