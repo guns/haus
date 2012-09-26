653c4d0d37d20ed79dcabf8c07f7addb87991452
guns <self@sungpae.com>
2012-09-26 14:21:27 -0500

    [init/iptables.sh] Large refactoring. More conntrack

--- share/init/iptables.sh
+++ share/init/iptables.sh
@@ -1,111 +1,116 @@
 #!/bin/sh
-# http://www.debiantutorials.net/loading-iptables-rules-on-startup/
-# http://www.brandonhutchinson.com/iptables_fw.html
+#
+#     .                     s                    ..         ..             .x+=:.
+#    @88>                  :8              . uW8"     x .d88"             z`    ^%
+#    %8P   .d``           .88              `t888       5888R                 .   <k
+#     .    @8Ne.   .u    :888ooo      u     8888   .   '888R       .u      .@8Ned8"
+#   .@88u  %8888:u@88N -*8888888   us888u.  9888.z88N   888R    ud8888.  .@^%8888"
+#  ''888E`  `888I  888.  8888   .@88 "8888" 9888  888E  888R  :888'8888.x88:  `)8b.
+#    888E    888I  888I  8888   9888  9888  9888  888E  888R  d888 '88%"8888N=*8888
+#    888E    888I  888I  8888   9888  9888  9888  888E  888R  8888.+"    %8"    R88
+#    888E  uW888L  888' .8888Lu=9888  9888  9888  888E  888R  8888L       @8Wou 9%
+#    888& '*88888Nu88P  ^%888*  9888  9888 .8888  888" .888B .'8888c. .+.888888P`
+#    R888"~ '88888F`      'Y"   "888*""888" `%888*%"   ^*888%  "88888%  `   ^"F
+#     ""     888 ^               ^Y"   ^Y'     "`        "%      "YP'
+#            *8E
+#            '8>                                          guns <self@sungpae.com>
+#             "
+#
+#  cf. http://inai.de/documents/Perfect_Ruleset.pdf
+#
+#  Note that this file should not be used directly as an init script. Set the
+#  firewall state once with this script, then dump with iptables-save.
+
+set -e
 
 printf 'Loading iptables rules... '
 
-### Initialization
-
-set -e
+#
+# Initialization
+#
 
 test -n "$IPTABLES" || IPTABLES=$(command -v iptables)
 test -x "$IPTABLES" || { echo "Could not execute $IPTABLES"; exit 1; }
 
-# Disable IPv6
+# Flush rules and delete non-default chains
+for TABLE in filter nat mangle raw security; do
+    $IPTABLES --table $TABLE --flush
+    $IPTABLES --table $TABLE --delete-chain
+done
+
+# Default policies
+$IPTABLES --policy INPUT   DROP
+$IPTABLES --policy FORWARD DROP
+$IPTABLES --policy OUTPUT  ACCEPT
+
+# Disable IPv6 until hell freezes over
 test -n "$IP6TABLES" || IP6TABLES=$(command -v ip6tables)
 test -x "$IP6TABLES" && test -e /proc/net/if_inet6 && {
     printf 'Filtering IPv6... '
-    $IP6TABLES -t filter --flush
-    $IP6TABLES -t mangle --flush
-    $IP6TABLES -t raw    --flush
-    $IP6TABLES -t filter --delete-chain
-    $IP6TABLES -t mangle --delete-chain
-    $IP6TABLES -t raw    --delete-chain
-    $IP6TABLES -P INPUT   DROP
-    $IP6TABLES -P FORWARD DROP
-    $IP6TABLES -P OUTPUT  DROP
+    for TABLE in filter mangle raw security; do
+        $IP6TABLES --table $TABLE --flush
+        $IP6TABLES --table $TABLE --delete-chain
+    done
+    $IP6TABLES --policy INPUT   DROP
+    $IP6TABLES --policy FORWARD DROP
+    $IP6TABLES --policy OUTPUT  DROP
 }
 
-# Flush rules and delete non-default chains
-$IPTABLES -t filter --flush
-$IPTABLES -t nat    --flush
-$IPTABLES -t mangle --flush
-$IPTABLES -t raw    --flush
-$IPTABLES -t filter --delete-chain
-$IPTABLES -t nat    --delete-chain
-$IPTABLES -t mangle --delete-chain
-$IPTABLES -t raw    --delete-chain
+# Packet tracing / logging
+# $IPTABLES --table raw --append PREROUTING/OUTPUT [--match ...] --jump TRACE
+$IPTABLES --new-chain LOGDROP
+$IPTABLES --append    LOGDROP   --jump LOG
+$IPTABLES --append    LOGDROP   --jump DROP
+$IPTABLES --new-chain LOGACCEPT
+$IPTABLES --append    LOGACCEPT --jump LOG
+$IPTABLES --append    LOGACCEPT --jump ACCEPT
 
-# Default policies
-$IPTABLES -P INPUT   DROP
-$IPTABLES -P FORWARD DROP
-$IPTABLES -P OUTPUT  ACCEPT
-
-# # Convenient packet logging
-# $IPTABLES -N LOGDROP
-# $IPTABLES -A LOGDROP -j LOG
-# $IPTABLES -A LOGDROP -j DROP
-# $IPTABLES -N LOGACCEPT
-# $IPTABLES -A LOGACCEPT -j LOG
-# $IPTABLES -A LOGACCEPT -j ACCEPT
-
-
-### Core rules
-
-# Loopback access
-$IPTABLES -A INPUT  -i lo -j ACCEPT
-$IPTABLES -A OUTPUT -o lo -j ACCEPT
+#
+# Core rules
+#
 
 # Stateful rule
-$IPTABLES -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+$IPTABLES --append INPUT --match conntrack --ctstate ESTABLISHED --jump ACCEPT
+
+# Loopback access
+$IPTABLES --append INPUT  --in-interface  lo --jump ACCEPT
+# $IPTABLES --append OUTPUT --out-interface lo --jump ACCEPT # Uncomment if OUTPUT policy is DROP
 
 # ICMP
-$IPTABLES -A INPUT -p icmp -m state --state NEW -j ACCEPT
+$IPTABLES --append INPUT --protocol icmp --match conntrack --ctstate NEW,RELATED --jump ACCEPT
 
+#
+# Security
+#
 
-### Services
+$IPTABLES --append INPUT --match conntrack --ctstate INVALID --jump LOGDROP
 
+#
+# Services
+#
 
-### Security
+# Host based exception
+# $IPTABLES --append INPUT --source VMHOST --match conntrack --ctstate NEW --jump ACCEPT
 
+# SSH
+# $IPTABLES --append INPUT --protocol tcp --dport 22 --match conntrack --ctstate NEW --jump ACCEPT
 
-### Examples
+# HTTP
+# $IPTABLES --append INPUT --protocol tcp --dport 80  --match conntrack --ctstate NEW --jump ACCEPT
+# $IPTABLES --append INPUT --protocol tcp --dport 443 --match conntrack --ctstate NEW --jump ACCEPT
+# $IPTABLES --append INPUT --protocol tcp --match multiport --dports 80,443 --match conntrack --ctstate NEW --jump ACCEPT
 
-# # Host based exception
-# $IPTABLES -A INPUT --source VMHOST -m state --state NEW -j ACCEPT
+# NFS
+# $IPTABLES --append INPUT --protocol tcp --dport 111   --match conntrack --ctstate NEW --jump ACCEPT
+# $IPTABLES --append INPUT --protocol udp --dport 111   --match conntrack --ctstate NEW --jump ACCEPT
+# $IPTABLES --append INPUT --protocol tcp --dport 2049  --match conntrack --ctstate NEW --jump ACCEPT
+# $IPTABLES --append INPUT --protocol tcp --dport 32767 --match conntrack --ctstate NEW --jump ACCEPT
 
-# # SSH
-# $IPTABLES -A INPUT -p tcp --dport 22 -m state --state NEW -j ACCEPT
+# NTP
+# $IPTABLES --append INPUT --protocol udp --dport 123 --jump ACCEPT
 
-# # HTTP
-# $IPTABLES -A INPUT -p tcp --dport 80  -m state --state NEW -j ACCEPT
-# $IPTABLES -A INPUT -p tcp --dport 443 -m state --state NEW -j ACCEPT
-
-# # NFS
-# $IPTABLES -A INPUT -p tcp --dport 111   -m state --state NEW -j ACCEPT
-# $IPTABLES -A INPUT -p udp --dport 111   -m state --state NEW -j ACCEPT
-# $IPTABLES -A INPUT -p tcp --dport 2049  -m state --state NEW -j ACCEPT
-# $IPTABLES -A INPUT -p tcp --dport 32767 -m state --state NEW -j ACCEPT
-
-# # NTP
-# $IPTABLES -A INPUT -p udp --dport 123 -j ACCEPT
-
-# # Samba
-# $IPTABLES -A INPUT -p tcp -m multiport --dports 139,445 -m state  --state NEW -j ACCEPT
-# $IPTABLES -A INPUT -p udp -m multiport --dports 137,138 -j ACCEPT
-
-# # Block new connections without SYN
-# $IPTABLES -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
-
-# # Block fragments and Xmas tree as well as SYN,FIN and SYN,RST
-# $IPTABLES -A INPUT -p ip -f -j DROP
-# $IPTABLES -A INPUT -p tcp --tcp-flags ALL     ACK,RST,SYN,FIN -j DROP
-# $IPTABLES -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN         -j DROP
-# $IPTABLES -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST         -j DROP
-
-# # Anti-spoofing rules
-# $IPTABLES -A INPUT -s 200.200.200.200 -j DROP
-# $IPTABLES -A INPUT -s 192.168.0.0/24  -j DROP
-# $IPTABLES -A INPUT -s 127.0.0.0/8     -j DROP
+# Samba
+# $IPTABLES --append INPUT --protocol tcp --match multiport --dports 139,445 --match conntrack --ctstate NEW --jump ACCEPT
+# $IPTABLES --append INPUT --protocol udp --match multiport --dports 137,138 --match conntrack --ctstate NEW --jump ACCEPT
 
 echo 'OK'
