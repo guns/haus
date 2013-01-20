caa504f7ff35feb45d0fcd2b5486024ea4b3f47e
guns <self@sungpae.com>
2013-01-19 20:44:37 -0600

    [bash] pass aliases

--- etc/bashrc.d/interactive.bash
+++ etc/bashrc.d/interactive.bash
@@ -1359,9 +1359,10 @@ ALIAS ssl='openssl' && {
 ALIAS gpg='gpg2 --no-encrypt-to' || ALIAS gpg='gpg --no-encrypt-to'
 
 # pass
-ALIAS pclip='pass -c' && {
-    passl() { pass "$@" | pager; }
-    TCOMP pass passl
+ALIAS passc='pass -c' && {
+    passl() { pass "$@" | pager; }; TCOMP pass passl
+    passi() { pass insert -fm "$1" < <(genpw "${@:2}"); pass "$1"; }; TCOMP pass passi
+    passic() { pass insert -fm "$1" < <(genpw "${@:2}"); pass -c "$1"; }; TCOMP pass passic
 }
 
 # cryptsetup
