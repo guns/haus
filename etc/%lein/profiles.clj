f2e7d47ea5de2eb75e2a6aed267753b8b582e166
guns <self@sungpae.com>
2013-08-16 21:00:59 -0500

    [leiningen] Update profile

--- etc/%lein/profiles.clj
+++ etc/%lein/profiles.clj
@@ -19,7 +19,7 @@
 ;;        guns <self@sungpae.com>                    /######  /#
 ;;                                                  /     ###/
 
-{:user {:plugins [[lein-exec "0.3.0"]
+{:user {:plugins [[lein-exec "0.3.1"]
                   [lein-kibit "0.0.8"]
                   [lein-ancient "0.4.4"]]
         :dependencies [[org.clojure/tools.trace "0.7.5"]
@@ -28,52 +28,51 @@
                   "REPL" ["trampoline" "repl" ":headless"]}
         :signing {:gpg-key "0x24142BFA974864C311D3AED537BD7E31C8C835A2"}
         :repl-options
-        {:init (do
-                 ;;
-                 ;; Swap javadoc URLs to local versions
-                 ;;
+        {:init
+         (do
+           ;;
+           ;; Swap javadoc URLs to local versions
+           ;;
 
-                 (require 'clojure.java.javadoc)
+           (require 'clojure.java.javadoc)
 
-                 (let [core-url clojure.java.javadoc/*core-java-api*
-                       local-url "http://api/jdk7/api/"]
-                   (dosync
-                     (alter clojure.java.javadoc/*remote-javadocs*
-                            #(reduce
-                               (fn [m [pre url]]
-                                 (assoc m pre (if (= url core-url)
-                                                local-url
-                                                url)))
-                               {} %)))
-                   (alter-var-root #'clojure.java.javadoc/*core-java-api*
-                                   (constantly local-url)))
+           (let [core-url clojure.java.javadoc/*core-java-api*
+                 local-url "http://api/jdk7/api/"]
+             (dosync
+               (alter clojure.java.javadoc/*remote-javadocs*
+                      #(reduce
+                         (fn [m [pre url]]
+                           (assoc m pre (if (= url core-url)
+                                          local-url
+                                          url)))
+                         {} %)))
+             (alter-var-root #'clojure.java.javadoc/*core-java-api*
+                             (constantly local-url)))
 
-                 ;;
-                 ;; Add debugging macros
-                 ;;
+           ;;
+           ;; Add debugging macros
+           ;;
 
-                 (require 'clojure.pprint)
+           (require 'clojure.pprint)
 
-                 (defmacro debug
-                   ([x]
-                    `(do (clojure.pprint/pprint {'~x ~x}) ~x))
-                   ([x & xs]
-                    `(do (clojure.pprint/pprint (zipmap '~(reverse (cons x xs))
-                                                        [~@(reverse (cons x xs))]))
-                         ~(last xs))))
+           (defmacro debug
+             ([& xs]
+              `(do (clojure.pprint/pprint
+                     (zipmap '~(reverse xs) [~@(reverse xs)]))
+                   ~(last xs))))
 
-                 (defmacro dump-locals []
-                   `(clojure.pprint/pprint
-                      ~(into {} (map (fn [l] [`'~l l]) (reverse (keys &env))))))
+           (defmacro dump-locals []
+             `(clojure.pprint/pprint
+                ~(into {} (map (fn [l] [`'~l l]) (reverse (keys &env))))))
 
-                 (defmacro benchmark
-                   ([expr] `(benchmark 10 ~expr))
-                   ([n expr] `(time (dotimes [_# ~n] ~expr))))
+           (defmacro benchmark
+             ([expr] `(benchmark 10 ~expr))
+             ([n expr] `(time (dotimes [_# ~n] ~expr))))
 
-                 (defmacro trace
-                   ([expr] `(trace *ns* ~expr))
-                   ([nspace expr]
-                    (require 'clojure.tools.trace)
-                    `(try (clojure.tools.trace/trace-ns ~nspace)
-                          ~expr
-                          (finally (clojure.tools.trace/untrace-ns ~nspace))))))}}}
+           (defmacro trace
+             ([expr] `(trace *ns* ~expr))
+             ([nspace expr]
+              (require 'clojure.tools.trace)
+              `(try (clojure.tools.trace/trace-ns ~nspace)
+                    ~expr
+                    (finally (clojure.tools.trace/untrace-ns ~nspace))))))}}}
