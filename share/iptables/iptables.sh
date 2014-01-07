3f69305b6802ecfbe4334a09c0d76489a303b165
guns <self@sungpae.com>
2014-01-06 19:03:11 -0600

    [iptables] Refactor iptables script

--- share/iptables/iptables.sh
+++ share/iptables/iptables.sh
@@ -17,6 +17,7 @@
 #             "
 #
 #  cf. http://inai.de/documents/Perfect_Ruleset.pdf
+#      http://inai.de/images/nf-packet-flow.png
 #
 #  Note that this file should not be used directly as an init script. Set the
 #  firewall state once with this script, then dump with iptables-save.
@@ -29,88 +30,105 @@ printf 'Loading iptables rules... '
 # Initialization
 #
 
-test -n "$IPTABLES" || IPTABLES=$(command -v iptables)
-test -x "$IPTABLES" || { echo "Could not execute $IPTABLES"; exit 1; }
+test -n "$IPTABLES" || IPTABLES="$(command -v iptables)"
+test -x "$IPTABLES" || { echo "Could not execute $IPTABLES" >&2; exit 1; }
+
+iptables() { "$IPTABLES" "$@"; }
 
 # Flush rules and delete non-default chains
-for TABLE in filter nat mangle raw security; do
-    "$IPTABLES" --table $TABLE --flush
-    "$IPTABLES" --table $TABLE --delete-chain
+for TABLE in raw mangle nat filter security; do
+    iptables --table "$TABLE" --flush
+    iptables --table "$TABLE" --delete-chain
 done
 
-# Default policies
-"$IPTABLES" --policy INPUT   DROP
-"$IPTABLES" --policy FORWARD DROP
-"$IPTABLES" --policy OUTPUT  ACCEPT
+# Set default policies
+iptables --policy INPUT   DROP
+iptables --policy FORWARD DROP
+iptables --policy OUTPUT  ACCEPT
 
-# Disable IPv6 until hell freezes over
-test -n "$IP6TABLES" || IP6TABLES=$(command -v ip6tables)
+# Block IPv6 until hell freezes over
+test -n "$IP6TABLES" || IP6TABLES="$(command -v ip6tables)"
 test -x "$IP6TABLES" && test -e /proc/net/if_inet6 && {
     printf 'Filtering IPv6... '
-    for TABLE in filter mangle raw security; do
-        $IP6TABLES --table $TABLE --flush
-        $IP6TABLES --table $TABLE --delete-chain
+    for TABLE in raw mangle filter security; do
+        "$IP6TABLES" --table "$TABLE" --flush
+        "$IP6TABLES" --table "$TABLE" --delete-chain
     done
-    $IP6TABLES --policy INPUT   DROP
-    $IP6TABLES --policy FORWARD DROP
-    $IP6TABLES --policy OUTPUT  DROP
+    "$IP6TABLES" --policy INPUT   DROP
+    "$IP6TABLES" --policy FORWARD DROP
+    "$IP6TABLES" --policy OUTPUT  DROP
 }
+
+unset TABLE
 
 #
 # Chains
 #
 
-# "$IPTABLES" --table raw --append PREROUTING/OUTPUT [--match ...] --jump TRACE
-
-# "$IPTABLES" --new-chain DROP-INVALID
-# "$IPTABLES" --append    DROP-INVALID --jump LOG  --log-prefix '[INPUT INVALID] '
-# "$IPTABLES" --append    DROP-INVALID --jump DROP
+iptables --new-chain DROPINV
+iptables --append    DROPINV --jump LOG  --log-prefix '[DROPINV] '
+iptables --append    DROPINV --jump DROP
 
 #
-# Core rules
+# Inbound rules
 #
 
-# Stateful rule
-"$IPTABLES" --append INPUT --match conntrack --ctstate ESTABLISHED --jump ACCEPT
+# Allow loopback traffic
+iptables --append INPUT  --in-interface  lo --jump ACCEPT
+# iptables --append OUTPUT --out-interface lo --jump ACCEPT
 
-# Inbound loopback
-"$IPTABLES" --append INPUT  --in-interface lo --jump ACCEPT
+# Allow established traffic
+iptables --append INPUT --match conntrack --ctstate ESTABLISHED --jump ACCEPT
 
-# Outbound loopback; Uncomment if OUTPUT policy is DROP
-# "$IPTABLES" --append OUTPUT --out-interface lo --jump ACCEPT
+# Allow ICMP
+iptables --append INPUT --protocol icmp --match conntrack --ctstate NEW,RELATED --jump ACCEPT
 
-# ICMP
-"$IPTABLES" --append INPUT --protocol icmp --match conntrack --ctstate NEW,RELATED --jump ACCEPT
+# Drop invalid input
+iptables --append INPUT --match conntrack --ctstate INVALID --jump DROPINV
 
-#
-# Security
-#
+### Services
 
-"$IPTABLES" --append INPUT --match conntrack --ctstate INVALID --jump DROP
-
-#
-# Services
-#
-
-# Host based exception
-# "$IPTABLES" --append INPUT --source VMHOST --match conntrack --ctstate NEW --jump ACCEPT
+accept_new() { "$IPTABLES" --append INPUT "$@" --match conntrack --ctstate NEW --jump ACCEPT; }
 
 # SSH
-# "$IPTABLES" --append INPUT --protocol tcp --dport 22 --match conntrack --ctstate NEW --jump ACCEPT
+# accept_new --protocol tcp --dport 22
 
 # HTTP
-# "$IPTABLES" --append INPUT --protocol tcp --dport 80  --match conntrack --ctstate NEW --jump ACCEPT
-# "$IPTABLES" --append INPUT --protocol tcp --dport 443 --match conntrack --ctstate NEW --jump ACCEPT
-# "$IPTABLES" --append INPUT --protocol tcp --match multiport --dports 80,443 --match conntrack --ctstate NEW --jump ACCEPT
+# accept_new --protocol tcp --dport 80
+# accept_new --protocol tcp --dport 443
+# accept_new --protocol tcp --match multiport --dports 80,443
+
+# DHCP
+# accept_new --protocol udp --in-interface eth0 --sport 67:68 --dport 67:68
+
+# DNS
+# accept_new --protocol udp --in-interface eth0 --dport 53
 
 # NFS
-# "$IPTABLES" --append INPUT --protocol tcp --dport 111   --match conntrack --ctstate NEW --jump ACCEPT
-# "$IPTABLES" --append INPUT --protocol udp --dport 111   --match conntrack --ctstate NEW --jump ACCEPT
-# "$IPTABLES" --append INPUT --protocol tcp --dport 2049  --match conntrack --ctstate NEW --jump ACCEPT
-# "$IPTABLES" --append INPUT --protocol tcp --dport 32767 --match conntrack --ctstate NEW --jump ACCEPT
+# accept_new --protocol udp --dport 111
+# accept_new --protocol tcp --match multiport --dports 111,2049,32767
 
 # Samba
-# "$IPTABLES" --append INPUT --protocol tcp --match multiport --dports 139,445 --match conntrack --ctstate NEW --jump ACCEPT
-# "$IPTABLES" --append INPUT --protocol udp --match multiport --dports 137,138 --match conntrack --ctstate NEW --jump ACCEPT
+# accept_new --protocol udp --dport 137:138
+# accept_new --protocol tcp --match multiport --dports 139,445
+
+#
+# NAT
+#
+
+forward_interface() {
+    (($# == 2)) || return 1;
+    local in="$1" out="$2"
+    # Outbound
+    iptables --append FORWARD --in-interface "$in" --out-interface "$out" --jump ACCEPT
+    # Inbound
+    iptables --append FORWARD --in-interface "$out" --out-interface "$in"                 --match conntrack --ctstate ESTABLISHED --jump ACCEPT
+    iptables --append FORWARD --in-interface "$out" --out-interface "$in" --protocol icmp --match conntrack --ctstate NEW,RELATED --jump ACCEPT
+    iptables --append FORWARD --in-interface "$out" --out-interface "$in"                 --match conntrack --ctstate INVALID     --jump DROPINV
+    # Enable NAT
+    iptables --table nat --append POSTROUTING --out-interface "$out" --jump MASQUERADE
+}
+
+# forward_interface eth0 wlan0
 
 echo 'OK'
