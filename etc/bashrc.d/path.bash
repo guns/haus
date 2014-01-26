ac7b285963f9a110511a11e14905f8825f0eb871
guns <self@sungpae.com>
2014-01-26 17:54:10 -0600

    [bash] PATH += ~/.local/bin

--- etc/bashrc.d/path.bash
+++ etc/bashrc.d/path.bash
@@ -5,7 +5,7 @@
 # Requires ~/.bashrc.d/functions.bash
 
 PATH_ARY=(
-    ~/bin                               # User programs
+    ~/{.local/,}bin                     # User programs
     /usr/local/{,s}bin                  # Local admin programs
     "$RUBYPATH"                         # Used for switching top Ruby
     /opt/ruby/{2.0,1.9,1.8,1.8.6}/bin   # Ruby installations
