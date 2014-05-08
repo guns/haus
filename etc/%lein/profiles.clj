7578c05f87d4994dff1f44ade9c6c6001f19fa3f
guns <self@sungpae.com>
2014-05-08 01:14:10 -0500

    [vim][clojure] *print-length* and *print-level*

--- etc/%lein/profiles.clj
+++ etc/%lein/profiles.clj
@@ -41,7 +41,9 @@
                   "REPL" ["trampoline" "repl" ":headless"]
                   "slamhound" ["trampoline" "with-profile" "user,dev,slamhound" "run" "-m" "slam.hound"]}
         :signing {:gpg-key "0x4BC72AA6B1AE2B5AC7F7ADCF9D1AA266D2BC9C2D"}
-        :global-vars {*warn-on-reflection* true}
+        :global-vars {*warn-on-reflection* true
+                      *print-length* 500
+                      *print-level* 500}
         :jvm-opts ["-XX:+CMSClassUnloadingEnabled"]
         :repl-options {:init-ns user
                        :init (do (load-file (str (System/getProperty "user.home")
