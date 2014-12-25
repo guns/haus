d51cde03b6461154663789a32cf35e36637c0a48
guns <self@sungpae.com>
2014-12-25 03:34:05 -0600

    [genpw][util/password] Allow streaming from stdin

--- lib/ruby/util/password.rb
+++ lib/ruby/util/password.rb
@@ -14,7 +14,7 @@ module Util
     ASCII = (0x21..0x7e).map &:chr # Printable non-whitespace characters
     ALPHA = ASCII.grep /[a-zA-Z0-9]/
 
-    # Open a file and stream it to a block, `bit_len` bits at a time.
+    # Stream an IO object to a block, `bitlen` bits at a time.
     #
     # Note that this is effectively an implementation of `rand(ceil)`, where
     # `ceil` is a power of 2 and the entropy source is an arbitrary file.
@@ -22,9 +22,11 @@ module Util
     # Example:
     #
     #   count = 0
-    #   stream '/dev/random', 12 do |n|
-    #     puts '%4d: %012b' % [n, n]
-    #     break if (count += 1) > 3
+    #   File.open '/dev/random', 'r' do |f|
+    #     stream f, 12 do |n|
+    #       puts '%4d: %012b' % [n, n]
+    #       break if (count += 1) > 3
+    #     end
     #   end
     #
     # outputs:
@@ -34,41 +36,48 @@ module Util
     #   1847: 011100110111
     #   1921: 011110000001
     #
-    def stream source, bit_len = 8
+    def stream io, bitlen = 8
       buf = len = 0
-      File.open source, 'r' do |f|
-        loop do
-          until len >= bit_len
-            byte = f.getbyte
-            raise 'Source reached EOF' if byte.nil?
-            len += 8
-            buf <<= 8
-            buf |= byte
-            # puts "byte:  %08b\nbuf:   %0#{len}b (%d/%d)\n" % [byte, buf, len, bit_len]
-          end
-          len -= bit_len
-          # puts "FLUSH: %0#{bit_len}b" % (buf >> len)
-          yield buf >> len
-          buf &= ~(~0 << len)
-          # puts "buf:   %0#{len}b (%d/%d)" % [buf, len, bit_len]
+      loop do
+        until len >= bitlen
+          byte = io.getbyte
+          raise EOFError if byte.nil?
+          len += 8
+          buf <<= 8
+          buf |= byte
+          # puts "byte:  %08b\nbuf:   %0#{len}b (%d/%d)\n" % [byte, buf, len, bitlen]
         end
+        len -= bitlen
+        # puts "FLUSH: %0#{bitlen}b" % (buf >> len)
+        yield buf >> len
+        buf &= ~(~0 << len)
+        # puts "buf:   %0#{len}b (%d/%d)" % [buf, len, bitlen]
       end
     end
 
     def password length = PASSWORD_LENGTH, opts = {}
-      source   = opts[:source] || '/dev/urandom'
-      chars    = opts[:chars ] || ASCII
-      size     = chars.size
-      bit_len  = Math.log(size, 2).ceil
-      buf, idx = [], 0
+      source = opts[:source] || '/dev/urandom'
+      chars  = opts[:chars ] || ASCII
+      size   = chars.size
+      bitlen = Math.log(size, 2).ceil
+      buf    = []
+      idx    = 0
+      close  = false
 
-      stream source, bit_len do |n|
+      if source.is_a? String
+        close = true
+        source = File.open source, 'r'
+      end
+
+      stream source, bitlen do |n|
         next if n >= size
         buf[idx] = chars[n]
         break if (idx += 1) >= length
       end
 
       buf.join
+    ensure
+      source.close if close
     end
   end
 end
