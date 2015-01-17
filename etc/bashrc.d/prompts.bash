827cbe46ea3ce0e546c28ee77d7f0dab9e11cd76
guns <self@sungpae.com>
2015-01-16 23:14:44 -0600

    [bash] Don't unset PS_{COLOR,DELIM}

--- etc/bashrc.d/prompts.bash
+++ etc/bashrc.d/prompts.bash
@@ -3,27 +3,27 @@
 ###
 
 if [[ "$SSH_TTY" ]]; then
-    PS_DELIM="${PS_DELIM:-■}" # SSH tunnel
+    __PS_DELIM__="${PS_DELIM:-■}" # SSH tunnel
     if ((EUID > 0)); then
-        PS_COLOR="${PS_COLOR:-36}"
+        __PS_COLOR__="${PS_COLOR:-36}"
     else
-        PS_COLOR="${PS_COLOR:-31}"
+        __PS_COLOR__="${PS_COLOR:-31}"
     fi
 else
-    PS_DELIM="${PS_DELIM:-§}" # Section
+    __PS_DELIM__="${PS_DELIM:-§}" # Section
     if ((EUID > 0)); then
-        PS_COLOR="${PS_COLOR:-37}"
+        __PS_COLOR__="${PS_COLOR:-37}"
     else
-        PS_COLOR="${PS_COLOR:-35}"
+        __PS_COLOR__="${PS_COLOR:-35}"
     fi
 fi
 
 # Interactive prompts need to surround escapes with RL_PROMPT_*_IGNORE,
 # which have special escapes in Bash: \[ \]
-export PS1="\[\e[0;${PS_COLOR}m\]\\H \[\e[1m\]${PS_DELIM}\[\e[22m\] \\w\\n\[\e[0;${PS_COLOR}m\]\\u\\$\[\e[0m\] "
-export PS2="\[\e[0;${PS_COLOR}m\]${USER//?/░}░\[\e[0m\] "
+export PS1="\[\e[0;${__PS_COLOR__}m\]\\H \[\e[1m\]${__PS_DELIM__}\[\e[22m\] \\w\\n\[\e[0;${__PS_COLOR__}m\]\\u\\$\[\e[0m\] "
+export PS2="\[\e[0;${__PS_COLOR__}m\]${USER//?/░}░\[\e[0m\] "
 export PS4='• '
-unset PS_DELIM PS_COLOR
+unset __PS_DELIM__ __PS_COLOR__
 
 # Show exit status of last command if non-zero
 __EXITSTATUS__() {
