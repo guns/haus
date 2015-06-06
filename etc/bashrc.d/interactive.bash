df7668b729cef5774d019996874557e7820fb1e8
guns <self@sungpae.com>
2015-06-05 20:49:43 -0500

    [bash] ytvlc

--- etc/bashrc.d/interactive.bash
+++ etc/bashrc.d/interactive.bash
@@ -1344,7 +1344,8 @@ HAVE pulseaudio && {
 
 # youtube-dl
 HAVE youtube-dl && {
-    alias yt='youtube-dl --output "%(title)s" --user-agent "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36"'
+    yt()    { youtube-dl --output "%(title)s" --user-agent "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36" "$@"; }
+    ytvlc() { youtube-dl --output -           --user-agent "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36" "$@"  | vlc -; }
     TCOMP youtube-dl yt
 }
 
