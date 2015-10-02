222315cb450f74e55e657c503721a63b8245c20f
guns <self@sungpae.com>
2015-10-02 16:25:37 -0500

    [*] Update UA

--- lib/ruby/nerv/cli/repl_helpers.rb
+++ lib/ruby/nerv/cli/repl_helpers.rb
@@ -80,7 +80,7 @@ module NERV::CLI::ReplHelpers
 
   def slurp path
     if path =~ %r{\A\w+://}
-      IO.popen(['curl', '--user-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36', '--progress-bar', '--location', path]) { |io| io.read }
+      IO.popen(['curl', '--user-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36', '--progress-bar', '--location', path]) { |io| io.read }
     else
       File.read path
     end
