99b8f19e1dbbf67445460801533c851f5f14ba4c
guns <self@sungpae.com>
2015-11-10 22:11:21 -0600

    [*] UA

--- etc/bashrc.d/interactive.bash
+++ etc/bashrc.d/interactive.bash
@@ -622,8 +622,8 @@ ALIAS net='netctl' \
       netstop='netctl stop-all'
 
 # cURL
-ALIAS get='curl --user-agent "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36" --progress-bar --location' \
-      geto='curl --user-agent "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36" --progress-bar --location --remote-name'
+ALIAS get='curl --user-agent "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36" --progress-bar --location' \
+      geto='curl --user-agent "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36" --progress-bar --location --remote-name'
 
 # dig
 ALIAS digx='dig -x' \
