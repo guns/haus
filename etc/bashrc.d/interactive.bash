03756f5dacbad3d36a1bf58285b3a3b21fd3d3c4
guns <self@sungpae.com>
2016-06-25 20:06:32 -0500

    [feh][bash] feh --theme DIR

--- etc/bashrc.d/interactive.bash
+++ etc/bashrc.d/interactive.bash
@@ -1370,8 +1370,8 @@ ALIAS geometry='identify -format "%w %h"'
 
 # feh
 HAVE feh && {
-    ALIAS fshow='feh --recursive' \
-          frand='feh --recursive --randomize' \
+    ALIAS fshow='feh --theme=DIR --recursive' \
+          frand='feh --theme=DIR --recursive --randomize' \
           ftime='feh --sort mtime'
     alias fmove='fehmove'
     alias fcopy='fehmove --copy'
