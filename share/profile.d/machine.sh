b3ca9d87c08aad19c24b846ec77cfa4046d0f86a
guns <self@sungpae.com>
2016-06-25 21:00:05 -0500

    [share] Update machine.sh

--- share/profile.d/machine.sh
+++ share/profile.d/machine.sh
@@ -1,20 +1,20 @@
 ### CONTAINER ENVIRONMENT HACKS
 
-# Use host's X server
-export DISPLAY=':0'
-
-# We will never connect from a basic terminal emulator
-if [[ "$TERM" == vt220 ]]; then
-    export TERM='rxvt-unicode-256color'
+if ((EUID == 0)) && [[ "$TERM" == vt220 ]]; then
+    while read -r -d $'\0' kv; do
+        if [[ "${kv:0:5}" == "TERM=" ]]; then
+            export "$kv"
+            break
+        fi
+    done < /proc/1/environ
 fi
 
-# Use different prompt colors
-export PS_DELIM='◩'
 if ((EUID == 0)); then
-    export PS_COLOR='38;5;227'
+    export PS_COLOR=33
 else
-    export PS_COLOR='38;5;243'
+    export PS_COLOR='38;5;244'
 fi
 
-# No /dev/shm host bind
+export PS_DELIM='◩'
+export DISPLAY=':0'
 export QT_X11_NO_MITSHM='1'
