43af09cf7a522f0798bc46a8380232667205eba2
guns <self@sungpae.com>
2016-09-01 23:09:05 -0500

    [*] UID -> EUID

--- etc/bashrc.d/env.bash
+++ etc/bashrc.d/env.bash
@@ -73,5 +73,5 @@ export WINEDLLOVERRIDES='winemenubuilder.exe=d'
 
 # GnuPG
 if [[ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne "$$" ]]; then
-    export SSH_AUTH_SOCK="/var/run/user/${SUDO_UID:-$UID}/gnupg/S.gpg-agent.ssh"
+    export SSH_AUTH_SOCK="/var/run/user/${SUDO_UID:-$EUID}/gnupg/S.gpg-agent.ssh"
 fi
