dbb6a5e2f00b675146af5586784f134ea530dc68
guns <self@sungpae.com>
2016-10-11 16:11:47 -0500

    [share] Remove read of /proc/1/environ
    Moved to nerv-services

--- share/profile.d/machine.sh
+++ share/profile.d/machine.sh
@@ -1,14 +1,5 @@
 ### CONTAINER ENVIRONMENT HACKS
 
-if ((EUID == 0)) && [[ "$TERM" == vt220 ]]; then
-    while read -r -d $'\0' kv; do
-        if [[ "${kv:0:5}" == "TERM=" ]]; then
-            export "$kv"
-            break
-        fi
-    done < /proc/1/environ
-fi
-
 if ((EUID == 0)); then
     export PS_COLOR=33
 else
@@ -16,5 +7,4 @@ else
 fi
 
 export PS_DELIM='◩'
-export DISPLAY=':0'
 export QT_X11_NO_MITSHM='1'
