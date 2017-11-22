d249c495b7235eced3659385c2337db415e89906
guns <self@sungpae.com>
2017-11-22 15:59:28 -0600

    [vim] Update splitjoin

--- etc/vim/bundle/splitjoin.vim/spec/plugin/rust_spec.rb
+++ etc/vim/bundle/splitjoin.vim/spec/plugin/rust_spec.rb
@@ -4,7 +4,7 @@ describe "rust" do
   let(:filename) { 'test.rs' }
 
   specify "match clauses" do
-    pending "Broken on TravisCI due to old Vim version"
+    pending "Broken on TravisCI due to old Vim version" if ENV['TRAVIS_CI']
 
     set_file_contents <<-EOF
       match one {
@@ -32,8 +32,61 @@ describe "rust" do
     EOF
   end
 
+  specify "question mark operator" do
+    pending "Broken on TravisCI due to old Vim version" if ENV['TRAVIS_CI']
+
+    set_file_contents <<-EOF
+      let file = File::open("foo.txt")?;
+    EOF
+
+    vim.search('File')
+    split
+
+    assert_file_contents <<-EOF
+      let file = match File::open("foo.txt") {
+          Ok(value) => value,
+          Err(e) => return Err(e.into()),
+      };
+    EOF
+
+    vim.search('File')
+    join
+
+    assert_file_contents <<-EOF
+      let file = File::open("foo.txt")?;
+    EOF
+  end
+
+  specify "complicated question mark operator" do
+    pending "Broken on TravisCI due to old Vim version" if ENV['TRAVIS_CI']
+
+    set_file_contents <<-EOF
+      let bar = foo + match write!("{}", floof) {
+          Ok(frob) => frob,
+          Err(err) => return Err(err),
+      } + 13;
+    EOF
+
+    vim.search('match')
+    join
+
+    assert_file_contents <<-EOF
+      let bar = foo + write!("{}", floof)? + 13;
+    EOF
+
+    vim.search('write')
+    split
+
+    assert_file_contents <<-EOF
+      let bar = foo + match write!("{}", floof) {
+          Ok(value) => value,
+          Err(e) => return Err(e.into()),
+      } + 13;
+    EOF
+  end
+
   specify "structs" do
-    pending "Broken on TravisCI due to old Vim version"
+    pending "Broken on TravisCI due to old Vim version" if ENV['TRAVIS_CI']
 
     set_file_contents <<-EOF
       SomeStruct { foo: bar, bar: baz }
@@ -57,7 +110,7 @@ describe "rust" do
   end
 
   specify "structs (trailing comma)" do
-    pending "Broken on TravisCI due to old Vim version"
+    pending "Broken on TravisCI due to old Vim version" if ENV['TRAVIS_CI']
 
     set_file_contents <<-EOF
       SomeStruct { foo: bar, bar: baz }
