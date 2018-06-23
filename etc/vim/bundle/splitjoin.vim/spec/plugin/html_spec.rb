a3d0f65644ae42dd6754efb49dff008f17d5c1c8
guns <self@sungpae.com>
2018-06-23 01:28:27 -0500

    [vim] Update splitjoin

--- etc/vim/bundle/splitjoin.vim/spec/plugin/html_spec.rb
+++ etc/vim/bundle/splitjoin.vim/spec/plugin/html_spec.rb
@@ -8,6 +8,11 @@ describe "html" do
     vim.set(:shiftwidth, 2)
   end
 
+  after :each do
+    vim.command('silent! unlet g:splitjoin_html_attributes_bracket_on_new_line')
+    vim.command('silent! unlet g:splitjoin_html_attributes_hanging')
+  end
+
   def simple_test(joined_html, split_html)
     set_file_contents joined_html
     split
@@ -36,6 +41,65 @@ describe "html" do
         token
         class="foo bar baz"
         style="width: 500px; height: 500px">
+    EOF
+
+    simple_test(joined_html, split_html)
+  end
+
+  specify "attributes in self-closing tags" do
+    joined_html = '<div id="test"/>'
+    split_html = <<-EOF
+      <div
+        id="test"/>
+    EOF
+
+    simple_test(joined_html, split_html)
+  end
+
+  specify "attributes in self-closing tags with bracket on new line" do
+    vim.command('let g:splitjoin_html_attributes_bracket_on_new_line = 1')
+
+    joined_html = '<div id="test" />'
+    split_html = <<-EOF
+      <div
+        id="test"
+        />
+    EOF
+
+    simple_test(joined_html, split_html)
+  end
+
+  specify "attributes with bracket on new line" do
+    vim.command('let g:splitjoin_html_attributes_bracket_on_new_line = 1')
+
+    joined_html = <<-EOF
+      <div id="test">
+      </div>
+    EOF
+    split_html = <<-EOF
+      <div
+        id="test"
+        >
+      </div>
+    EOF
+
+    simple_test(joined_html, split_html)
+  end
+
+  specify "hanging attributes" do
+    vim.command('let g:splitjoin_html_attributes_hanging = 1')
+
+    joined_html = <<-EOF
+      <button class="button control" @click="save" v-if="admin">
+        Save
+      </button>
+    EOF
+    split_html = <<-EOF
+      <button class="button control"
+              @click="save"
+              v-if="admin">
+        Save
+      </button>
     EOF
 
     simple_test(joined_html, split_html)
