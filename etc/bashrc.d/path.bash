5cbe5b96194064bc6c58837a0c15b1d404a83831
guns <self@sungpae.com>
2018-08-21 13:14:07 -0500

    [bash] Rewrite path.bash: 43ms -> 1ms

--- etc/bashrc.d/path.bash
+++ etc/bashrc.d/path.bash
@@ -2,24 +2,39 @@
 ### SHELL PATH
 ###
 
-PATHLIST=(
+__PATHLIST__=(
     ~/{.local/,}bin                     # User programs
     /usr/local/{,s}bin                  # Local admin programs
     {~/.haus/bin,/opt/haus/bin}         # Haus programs
     "$PATH"                             # Existing PATH
 )
 
+__IFS__=$IFS
+IFS=':' __PATHLIST__=(${__PATHLIST__[@]})
+IFS=$__IFS__
+
+__PATH__=''
+declare -A __PATHSET__=''
+
 # Prune duplicate, non-searchable, and non-extant directories, as well as
 # directories that are not owned by the current user or root.
-export PATH="$(ruby -e '
-    print ARGV.flat_map { |arg|
-        arg.split(":").map! { |p| File.symlink?(p) ? File.expand_path(File.readlink(p), File.dirname(p)) : p }
-    }.uniq!.select! { |path|
-        if File.directory? path and File.executable? path
-            stat = File.stat path
-            stat.uid == Process.euid or stat.uid.zero?
-        end
-    }.join(":")
-' -- "${PATHLIST[@]}")"
+for p in "${__PATHLIST__[@]}"; do
+    # Resolve symlinks
+    [[ -L "$p" ]] && p="$(realpath -e "$p")"
 
-unset PATHLIST
+    # Skip duplicates
+    [[ -z "${__PATHSET__["$p"]}" ]] || continue
+    __PATHSET__["$p"]=1
+
+    # Skip non-searchable directories
+    [[ -d "$p" && -x "$p" ]] || continue
+
+    # Skip directories owned by other non-root users
+    [[ -O "$p" ]] || [[ "$(stat -c %u "$p")" -eq 0 ]] || continue
+
+    __PATH__+="${p}:"
+done
+
+export PATH="${__PATH__%:}"
+
+unset __PATHLIST__ __IFS__ __PATH__ __PATHSET__ p
